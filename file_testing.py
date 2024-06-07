70018f4d89d46d0bd2b5961f90bd3ab4b165b1e0
mrzqi18 <161547549+mrzqi18@users.noreply.github.com>
2024-06-07 13:29:01 +0700

    Add files via upload

---
+++ file_testing.py
@@ -0,0 +1,9 @@
+import module
+
+text1 = "Lupakan Sasaran Pokuslah Pada Sistem - James Clear"
+text2 = "1% Untuk Menjadi Lebih Baik Dari Sekarang"
+text3 = "Mencium Wangi Uang Lebih Enak Dari Pada Mencium Cewe Yang Sudah Di Cium Banyak Orang - Timothy Ronald"
+
+print(f"Jumlah Huruf Besar Dalam Text 1 File Berbeda '{text1}' Adalah: {module.count_uppercase(text1)}")
+print(f"Jumlah Huruf Besar Dalam Text 2 File Berbeda '{text2}' Adalah: {module.count_uppercase(text2)}")
+print(f"Jumlah Huruf Besar Dalam Text 3 File Berbeda '{text3}' Adalah: {module.count_uppercase(text3)}")
